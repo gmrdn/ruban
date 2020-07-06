128d950b31be2708aa417102585360dee02e2c71
Guillaume R <guillaume.rondon@gmail.com>
2020-07-06 22:43:16 +0200

    Read the task arg and print it (with tests)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    task: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    greet_the_user(&mut std::io::stdout());
+    confirm_the_task(args.task, &mut std::io::stdout());
+}
+
+fn greet_the_user(mut writer: impl std::io::Write) {
+    writeln!(writer, "Hello, Ruban User.");
+}
+
+fn confirm_the_task(task: String, mut writer: impl std::io::Write) {
+    writeln!(writer, "{}", task);
+}
+
+#[test]
+fn should_greet_the_user() {
+    let mut result = Vec::new();
+    greet_the_user(&mut result);
+    assert_eq!(result, b"Hello, Ruban User.\n");
+}
+
+#[test]
+fn should_confirm_the_task() {
+    let task = String::from("etendre le linge");
+    let mut result = Vec::new();
+    confirm_the_task(task, &mut result);
+    assert_eq!(result, b"etendre le linge\n");
 }
