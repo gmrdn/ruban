7c1a1745183b3da2e449927129b0ea2d88ad45e6
Guillaume R <guillaume.rondon@gmail.com>
2020-07-09 11:21:01 +0200

    Reorganized rendering in a separate module and added ls subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::str::from_utf8;
+mod rendering;
+use crate::rendering::rendering::{confirm_the_tags, confirm_the_task, greet_the_user};
 use structopt::StructOpt;
-use std::error::Error;
 
 #[derive(StructOpt)]
 enum Ruban {
@@ -49,96 +49,23 @@ struct Task {
 #[derive(Debug)]
 enum Status {}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    greet_the_user(&mut std::io::stdout());
-    match Ruban::from_args() {
-        Ruban::Add { task, tags} => {
-            confirm_the_task(task, &mut std::io::stdout());
-            confirm_the_tags(tags, &mut std::io::stdout())
-        }
-        _ => Ok(()),
-    }
-}
 
-fn greet_the_user(mut writer: impl std::io::Write) -> Result<(), Box<dyn std::error::Error>> {
-    writeln!(writer, "Hello, Ruban User.")?;
-    Ok(())
-}
 
-fn confirm_the_task(
-    task: String,
-    mut writer: impl std::io::Write,
-) -> Result<(), Box<dyn std::error::Error>> {
-    writeln!(writer, "New task: {}", task)?;
-    Ok(())
-}
+const STD_OUT_ERR_MSG: &str = "Unable to write message in standard output";
 
-fn confirm_the_tags(
-    tags: String,
-    mut writer: impl std::io::Write,
-) -> Result<(), Box<dyn std::error::Error>> {
-    writeln!(writer, "Tags: {}", tags)?;
-    Ok(())
-}
-
-fn render_all_tasks(
-    tasks: &Tasks,
-    mut writer: impl std::io::Write,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for task in tasks {
-        writeln!(writer, "{} - {}", task.number, task.task)?;
-    }
-    Ok(())
-}
-
-#[test]
-fn should_greet_the_user() {
-    let mut result = Vec::new();
-    greet_the_user(&mut result);
-    assert_eq!(from_utf8(&result).unwrap(), "Hello, Ruban User.\n");
-}
-
-#[test]
-fn should_confirm_the_task() {
-    let task = String::from("Do the laundry");
-    let mut result = Vec::new();
-    confirm_the_task(task, &mut result);
-    assert_eq!(from_utf8(&result).unwrap(), "New task: Do the laundry\n");
-}
-
-#[test]
-fn should_confirm_the_tags() {
-    let tags = String::from("House");
-    let mut result = Vec::new();
-    confirm_the_tags(tags, &mut result);
-    assert_eq!(from_utf8(&result).unwrap(), "Tags: House\n");
-}
-
-#[test]
-fn should_display_all_tasks() {
+fn main() {
     let tasks = Tasks {
-        tasks: vec![
-            Task {
-                number: 1,
-                tags: Some("House".to_string()),
-                task: "Repair the garage door.".to_string(),
-                creation_date: None,
-                status: None,
-            },
-            Task {
-                number: 2,
-                tags: Some("Dev".to_string()),
-                task: "Finish the Rust Book.".to_string(),
-                creation_date: None,
-                status: None,
-            },
-        ],
-    };
-    let mut result = Vec::new();
+        tasks: vec![]};
 
-    render_all_tasks(&tasks, &mut result);
-    assert_eq!(
-        from_utf8(&result).unwrap(),
-        "1 - Repair the garage door.\n2 - Finish the Rust Book.\n"
-    );
+    greet_the_user(&mut std::io::stdout()).expect(STD_OUT_ERR_MSG);
+    match Ruban::from_args() {
+        Ruban::Add { task, tags } => {
+            confirm_the_task(task, &mut std::io::stdout()).expect(STD_OUT_ERR_MSG);
+            confirm_the_tags(tags, &mut std::io::stdout()).expect(STD_OUT_ERR_MSG);
+        },
+        Ruban::Ls {} => {
+            rendering::rendering::render_all_tasks(&tasks, &mut std::io::stdout()).expect(STD_OUT_ERR_MSG);
+        }
+        _ => ()
+    }
 }
---
+++ src/rendering.rs
@@ -0,0 +1,94 @@
+pub mod rendering {
+    use crate::Tasks;
+
+
+    pub fn greet_the_user(
+        mut writer: impl std::io::Write,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        writeln!(writer, "Hello, Ruban User.")?;
+        Ok(())
+    }
+
+    pub fn confirm_the_task(
+        task: String,
+        mut writer: impl std::io::Write,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        writeln!(writer, "New task: {}", task)?;
+        Ok(())
+    }
+
+    pub fn confirm_the_tags(
+        tags: String,
+        mut writer: impl std::io::Write,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        writeln!(writer, "Tags: {}", tags)?;
+        Ok(())
+    }
+
+    pub(crate) fn render_all_tasks(
+        tasks: &Tasks,
+        mut writer: impl std::io::Write,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        for task in tasks {
+            writeln!(writer, "{} - {}", task.number, task.task)?;
+        }
+        Ok(())
+    }
+
+    #[cfg(test)]
+    use crate::Task;
+    #[cfg(test)]
+    use std::str::from_utf8;
+
+    #[test]
+    fn should_greet_the_user() {
+        let mut result = Vec::new();
+        greet_the_user(&mut result);
+        assert_eq!(from_utf8(&result).unwrap(), "Hello, Ruban User.\n");
+    }
+
+    #[test]
+    fn should_confirm_the_task() {
+        let task = String::from("Do the laundry");
+        let mut result = Vec::new();
+        confirm_the_task(task, &mut result);
+        assert_eq!(from_utf8(&result).unwrap(), "New task: Do the laundry\n");
+    }
+
+    #[test]
+    fn should_confirm_the_tags() {
+        let tags = String::from("House");
+        let mut result = Vec::new();
+        confirm_the_tags(tags, &mut result);
+        assert_eq!(from_utf8(&result).unwrap(), "Tags: House\n");
+    }
+
+    #[test]
+    fn should_display_all_tasks() {
+        let tasks = Tasks {
+            tasks: vec![
+                Task {
+                    number: 1,
+                    tags: Some("House".to_string()),
+                    task: "Repair the garage door.".to_string(),
+                    creation_date: None,
+                    status: None,
+                },
+                Task {
+                    number: 2,
+                    tags: Some("Dev".to_string()),
+                    task: "Finish the Rust Book.".to_string(),
+                    creation_date: None,
+                    status: None,
+                },
+            ],
+        };
+        let mut result = Vec::new();
+
+        render_all_tasks(&tasks, &mut result);
+        assert_eq!(
+            from_utf8(&result).unwrap(),
+            "1 - Repair the garage door.\n2 - Finish the Rust Book.\n"
+        );
+    }
+}
